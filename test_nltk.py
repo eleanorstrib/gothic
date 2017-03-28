92b2f3fa3a7c9ac1552171bfc8f4a8a35f139132
eleanorstrib <hello@eleanorstrib.com>
2017-03-27 17:26:01 -0700

    some updated to the nltk file, filtering functions, removed extra imports

--- test_nltk.py
+++ test_nltk.py
@@ -3,56 +3,39 @@ Testing basic concepts & tools before doing our main analysis.
 """
 
 import string
-from collections import Counter
 from color_words import cw_raw
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-STOPWORDS = set(nltk.corpus.stopwords.words('english'))
 
-def generate_tokens():
+def tokenize_text():
     """
-    This function generates a list of tokens with stopwords and punctuation
-    removed.
+    This function generates a list of tokens with punctuation
+    and spaces removed for the whole text
     """
     text_tokens = []
     # open and read file
     text = open("./corpora/ShelleyMary_Frankenstein_Gutenberg.txt")
     for row in text:
         tokens = word_tokenize(row)# splits string
-        # puts everything in lowercase, removes stopwords and punctuation
-        tokens = [token.lower() for token in tokens if token not in STOPWORDS and token not in string.punctuation]
+        # puts everything in lowercase, removes punctuation
+        tokens = [token.lower() for token in tokens if token not in string.punctuation]
         # adds row tokens to master list
         text_tokens.extend(tokens)
     return text_tokens
 
-def counter(tokens):
-    """
-    This function counts the number of times a word appears in the list of tokens
-    and puts it into a dictionary.
-    """
-    word_count = Counter()
-    for word in tokens:
-        word_count[word] += 1
-    return word_count.most_common()
 
-def color_position(tokens, color_word_list):
+def word_count(tokens):
     """
-    Takes the tokenized list, lemmatizes the color words, and finds where in
-    the text they occur.
+    Counts the words in the text, excluding punctuation and spaces.
+    To be used with visualizations.
     """
-    word_map = {}
-    lemmatizer = WordNetLemmatizer()
-    for pos, word in enumerate(tokens):
-        lemmatized_word = lemmatizer.lemmatize(word)
-        print (lemmatized_word)
-        if lemmatized_word in color_word_list:
-            word_map[lemmatized_word] = word_map.get(lemmatized_word, []) + [pos]
-    print (word_map)
-    return word_map
+    words = len(tokens)
+    return words
 
-def word_type(tokens, cw_raw):
+
+def word_type(tokens):
     """
     Classifies the words in the corpus into types (e.g. noun, verb, etc.), then
     creates and returns lists of the nouns and adjectives.
@@ -60,29 +43,42 @@ def word_type(tokens, cw_raw):
     nouns = []
     adjectives  = []
     verbs = []
+    counter = 0
     # seperates tags into tuples in format ( word, tag)
     tagged_text =  nltk.pos_tag(tokens)
-    # loop through and add to appropriate list
-    for item in tagged_text:
-        if item[1][0] == "N":
-            nouns.append(item[0])
-        if item[1][0] == "J":
-            adjectives.append(item[0])
-        if item[1][0] == "V" and item[1][0] in cw_raw:
-            print(item)
-        else:
-            print('nope')
-    # deduplicate the lists
-    nouns = set(nouns)
-    adjectives = set(adjectives)
-    verbs = set(verbs)
-    return nouns
+    # loop through, determine word type, add position
+    for pos, item in enumerate(tagged_text):
+        try:
+            if item[1][0] == 'N':
+                nouns.append((item[0], pos))
+            if item[1][0] == 'J':
+                adjectives.append((item[0], pos))
+            if item[1][0] == 'V':
+                verbs.append((item[0], pos))
+        except:
+            print('exception from', item)
+    return nouns, adjectives, verbs
+
+
+def color_filter(typed_list, color_word_list):
+    """
+    Takes the tokenized list, lemmatizes the words, and determines if the root
+    word is in the color word list.
+    """
+    filtered = []
+    lemmatizer = WordNetLemmatizer()
+    for item in typed_list:
+        lemmatized_word = lemmatizer.lemmatize(item[0])
+        if lemmatized_word in color_word_list:
+            filtered.append(item)
+    return filtered
+
 
 def main():
-    tokens=generate_tokens()
-    counter(tokens)
-    color_position(tokens, cw_raw)
-    # word_type(tokens, cw_raw)
+    tokens = tokenize_text()
+    # word_count(tokens)
+    nouns, adjectives, verbs = word_type(tokens)
+    print("filtered list", color_filter(verbs, cw_raw))
 
 if __name__ == "__main__":
     main()
