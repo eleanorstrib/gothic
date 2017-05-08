c0eb66b051548c4dfdb072d0e0d740e17d5b286a
eleanorstrib <hello@eleanorstrib.com>
2017-05-08 12:19:56 -0700

    updated the debug var before putting online

--- gothicapp/gothicapp/settings.py
+++ gothicapp/gothicapp/settings.py
@@ -11,7 +11,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
