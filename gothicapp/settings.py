5026174ad3c7df500f144045c0d4249d48bde584
eleanorstrib <hello@eleanorstrib.com>
2017-05-08 14:02:30 -0700

    forgot a collect static var in settings

--- gothicapp/settings.py
+++ gothicapp/settings.py
@@ -124,3 +124,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static')
 )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
