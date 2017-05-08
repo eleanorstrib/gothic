0c5b9325e29ab5b47d746e7ba30d85e11a398ea5
eleanorstrib <hello@eleanorstrib.com>
2017-05-08 14:04:29 -0700

    fixed typo for STATICFILES_DIRS

--- gothicapp/settings.py
+++ gothicapp/settings.py
@@ -122,6 +122,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
+    [os.path.join(PROJECT_ROOT, 'static')]
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
