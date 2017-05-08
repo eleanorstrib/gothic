1053fe12718033fbe2bb9d3695288d8ace10096e
eleanorstrib <hello@eleanorstrib.com>
2017-05-08 13:43:03 -0700

    installed dj_db, updated reqs and settings

--- gothicapp/settings.py
+++ gothicapp/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,6 +73,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 FIXTURE_DIRS = (
    '/gothicapp/gothiccolors/fixtures/',
