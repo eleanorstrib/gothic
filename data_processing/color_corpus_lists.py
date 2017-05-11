b0d2ce56e7a5945871d2cf24a5fb33ea7de324e1
eleanorstrib <hello@eleanorstrib.com>
2017-05-11 15:04:13 -0700

    updated color_word_list to be a dict with hex value

--- data_processing/color_corpus_lists.py
+++ data_processing/color_corpus_lists.py
@@ -3,16 +3,18 @@ import csv
 
 def get_color_words():
     """
-    Gets color words from the csv file and puts them into a list.
+    Gets color words from the csv file and puts them into a dict where key = word
+    and value = hex value.
     """
-    color_word_list = []
+    color_word_dict = {}
     color_data = csv.reader(open('./color_names.csv'), delimiter=",", quotechar='"')
 
     for row in color_data:
         if row[0] != "Colour Name":
-            color_word_list.append(row[0].lower())
-
-    return color_word_list
+            name = row[0].lower()
+            hex_value = row[3].lower()
+            color_word_dict[name] = hex_value
+    return color_word_dict
 
 
 def get_corpus_filenames():
