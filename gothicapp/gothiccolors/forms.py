5abb37f7c3a3705e44202b4e0304ccc0da18b85c
eleanorstrib <hello@eleanorstrib.com>
2017-05-19 09:58:32 -0700

    changed form to use author instead of period as input

--- gothicapp/gothiccolors/forms.py
+++ gothicapp/gothiccolors/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from gothiccolors.models import Corpus
 
-class PeriodSearchForm(forms.ModelForm):
+class AuthorSearchForm(forms.ModelForm):
     class Meta:
         model = Corpus
-        fields = ('period',)
+        fields = ('author',)
