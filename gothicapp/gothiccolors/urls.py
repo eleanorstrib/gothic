c9a0adee4dcf3254e7686bd71d782c96dd01b24f
eleanorstrib <hello@eleanorstrib.com>
2017-05-19 22:02:23 -0700

    updated styling, added image, full palette menu item

--- gothicapp/gothiccolors/urls.py
+++ gothicapp/gothiccolors/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^results/$', views.results, name='results'),
+    url(r'^all_colors/$', views.all_colors, name='all_colors'),
 ]
